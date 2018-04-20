9cd90782b6d709bdb40d7437c4fc0c239df5a012
Rolando Javier <rjavier441@gmail.com>
2018-04-20 02:37:44 -0700

    Initial commit that initializes the car controller with a do-while loop

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+using namespace std;
+
+int main (void) {
+    bool on = false;
+
+    // Run car controller here
+    printf ("carController active\n");
+    do {
+
+    } while (on);
+
+    return 0;
+}
