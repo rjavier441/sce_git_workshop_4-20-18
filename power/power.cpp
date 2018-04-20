7d3a4c914ff10b4770d4e57dd7705f95a3c16352
nick <frangoose@gmail.com>
2018-04-20 16:47:57 -0700

    First commit

--- power/power.cpp
+++ power/power.cpp
@@ -2,7 +2,7 @@
 #include "power.h"
 using namespace std;
 
-int powerLevel = 32;
+int powerLevel = 32; //What does the scouter say?
 
 bool powerCheck() {
     // If battery level < 20, return false. Otherwise, return true
