6911aea750c86ff8fc31d7ae6cfc7a5b3a26d028
Nick <frangoose@gmail.com>
2018-04-20 17:51:21 -0700

    Defined functions in power/power.cpp (#5)
    * First commit
    
    * defined powerCheck in power.cpp
    
    * Added powerReset function
    
    * added void setPower(int pLevel) function
    
    * First commit
    
    * defined powerCheck in power.cpp
    
    * Added powerReset function
    
    * added void setPower(int pLevel) function

--- power/power.cpp
+++ power/power.cpp
@@ -2,9 +2,20 @@
 #include "power.h"
 using namespace std;
 
-int powerLevel = 32;
+int powerLevel = 32; //What does the scouter say?
 
 bool powerCheck() {
-    // If battery level < 20, return false. Otherwise, return true
-    return false;
+	// If battery level < 20, return false. Otherwise, return true
+	if(powerLevel < 20){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+void resetPower(){
+	powerLevel =32; //Back to inital state 
+}
+void setPower(int pLevel){
+    powerLevel =pLevel; 
 }
