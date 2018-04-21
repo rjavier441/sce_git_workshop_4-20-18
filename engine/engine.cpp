365b3dc32b79b03cda0cc3f7ff198de245e10661
drp <rtotop@gmail.com>
2018-04-20 17:19:21 -0700

    created lower temp setter method

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -11,10 +11,15 @@ int temperature = 50;
 bool engineIsOn () {
     // If engineFailure is true, return false. If engineActive is false, return false. Otherwise, return true.
     if (engineFailure) {return false;}
-    engineActive ? true : false;
+    return engineActive ? true : false;
 }
 
 void startEngine() {
     // If temperature < tempLo OR temperature > tempHi, set engineFailure to true.
     if (temperature < tempLo || temperature > tempHi) {engineFailure = true;}
+}
+
+void setTempLo(int newTempLo) {
+    // Sets the lower temperature threshold to a specified value
+    tempLo = newTempLo;
 }
