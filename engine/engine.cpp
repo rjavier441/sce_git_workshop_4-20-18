9c2863e72edaec7ea0b76b855bdb3341dc623081
drp <rtotop@gmail.com>
2018-04-20 17:05:20 -0700

    initial engine update

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -10,10 +10,11 @@ int temperature = 50;
 
 bool engineIsOn () {
     // If engineFailure is true, return false. If engineActive is false, return false. Otherwise, return true.
-    return false;
+    if (engineFailure) {return false;}
+    engineActive ? true : false;
 }
 
 void startEngine() {
     // If temperature < tempLo OR temperature > tempHi, set engineFailure to true.
-    return;
+    if (temperature < tempLo || temperature > tempHi) {engineFailure = true;}
 }
